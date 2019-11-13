8e26b0c396aff7dd07e3c2986d3a9a10bc97a26e
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-13 15:32:44 +0900

    챕터 1, 메인 페이지 구성

--- main.php
+++ main.php
@@ -45,14 +45,14 @@ $query2 = "select last_insert_id() as ID from accountTB order by ID desc limit 1
 //
 $result2 = $connect->query($query2) or die($this->_connect->error);
 
-$conn->close();
+
 
 while($row = $result2->fetch_array())
 {
     var_dump($row);
 }
 
-
+$conn->close();
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
