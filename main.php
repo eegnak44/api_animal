4d82839398c9956abbf8e8d8dddb60676e712bac
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-13 15:35:40 +0900

    챕터 1, 메인 페이지 구성

--- main.php
+++ main.php
@@ -49,10 +49,13 @@ $result2 = $conn->query($query2) or die($this->_connect->error);
 
 while($row = $result2->fetch_array())
 {
-    var_dump($row);
+    $id = $row['ID'];
 }
 
 $conn->close();
+
+
+echo $id;
 ?>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
