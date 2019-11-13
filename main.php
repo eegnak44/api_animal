f89fea15d67db5120bab14b5af401f18ff17d5b5
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-13 10:50:04 +0900

    project file commit

--- main.php
+++ main.php
@@ -35,15 +35,18 @@
             index = $(this).attr('value');
             // console.log(index);
 
-            if(index == 1){
-                // alert('1');
+            // if(index == 1){
+            //     // alert('1');
+            //     window.location.href = 'http://34.80.159.83/page_1-1.php?value='+index;
+            // } else if(index == 2){
+            //     // alert('2');
+            //     window.location.href = 'http://34.80.159.83/page_1-2.php?value='+index;
+            // } else if(index == 3){
+            //     // alert('3');
+            //     window.location.href = 'http://34.80.159.83/page_1-3.php?value='+index;
+            // }
+            if(index){
                 window.location.href = 'http://34.80.159.83/page_1-1.php?value='+index;
-            } else if(index == 2){
-                // alert('2');
-                window.location.href = 'http://34.80.159.83/page_1-2.php?value='+index;
-            } else if(index == 3){
-                // alert('3');
-                window.location.href = 'http://34.80.159.83/page_1-3.php?value='+index;
             }
         });
 
@@ -76,16 +79,17 @@
 
 </div>
 <form action="page_1-1.php" method="get">
-    <div class = 'choice_div'>
-        선택지 영역<br>
+    <div> class = 'choice_div'>
 <!--        <button type="button" name="button" id = 'choiceBtn1' value="1" >개가 병이 옮을 수 있으니, 격리하거나 안락사 시킨다. </button>-->
 <!--        <button type="button" name="button" id = 'choiceBtn2' value="2" >측은하긴 하지만, 개에게 까지 도움을 줄 여유가 없다.</button>-->
 <!--        <button type="button" name="button" id = 'choiceBtn3' value="3" >음식을 나누어주고 치료해준 후 떠난다.</button><br>-->
 <!--        <button type="button" name="button" id = 'choiceBtn4' value="4" >떠돌이 개를 다독여 함께 여행을 시작한다.</button><br>-->
-        <input type="radio" name ="button" value="1">개가 병이 옮을 수 있으니, 격리하거나 안락사 시킨다.</input>
-        <input type="radio" name ="button" value="2">측은하긴 하지만, 개에게 까지 도움을 줄 여유가 없다.</input>
-        <input type="radio" name ="button" value="3">음식을 나누어주고 치료해준 후 떠난다.</input>
-        <input type="radio" name ="button" value="4">떠돌이 개를 다독여 함께 여행을 시작한다.</input>
+        <input type="radio" name ="button" value="1">개가 병이 옮을 수 있으니, 격리하거나 안락사 시킨다.</input><br>
+        <input type="radio" name ="button" value="2">측은하긴 하지만, 개에게 까지 도움을 줄 여유가 없다.</input><br>
+        <input type="radio" name ="button" value="3">음식을 나누어주고 치료해준 후 떠난다.</input><br>
+        <input type="radio" name ="button" value="4">떠돌이 개를 다독여 함께 여행을 시작한다.</input><br>
+
+        <input type="submit" value="next">다음</input><br>
     </div>
 </form>
 <!--<div class = "testBtn1" value = '1' style="border-left: 1px grey;">선택지1</div>-->
