8e65ec9c626853bdd37ed01438c4ea2be7e8b63e
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-18 14:25:41 +0900

    결과 도출 수정

--- page_result.php
+++ page_result.php
@@ -193,6 +193,7 @@ function getResultGrade($value1, $value2, $value3, $value4, $value5, $arr){
     $resultStyle = ['고독한 여행자','버려진 외곽의 수호자','소외받은 이들의 동반자','광장의 경청자','작은방의 생태학자','용감한 사이보그','평범한 일반인','이상적 반려인'];
     $rdResultStyle = ['버려진 외곽의 수호자','소외받은 이들의 동반자','광장의 경청자','작은방의 생태학자','용감한 사이보그'];
     $tmpResultArr = ['0','0','0','0','0'];
+    $rdArr = Array();
 
     for($i = 0; $i < sizeof($arr); $i++){
         if($arr[$i] == 'A'){
@@ -203,7 +204,7 @@ function getResultGrade($value1, $value2, $value3, $value4, $value5, $arr){
             $j = 0;
             $tmpResultArr[$i] = '1';
             $gradeCCnt++;
-//            $rdArr[$j] = $rdResultStyle[$i];
+            $rdArr[$j] = $rdResultStyle[$i];
 //            if($gradeCCnt > 1){
 //                echo ($i+1)."번째 챕터에서 C";
 //            }
