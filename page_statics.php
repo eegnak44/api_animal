353653de1b7ed0f200a8b68f14bc96f2496b0994
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-18 12:50:18 +0900

    결과 도출 수정

--- page_statics.php
+++ page_statics.php
@@ -101,6 +101,8 @@ $resultNameCheck = 0;
 
 $query3 = "SELECT count(result_style) as cnt from play_dataTB WHERE result_style = '{$resultName}'";
 
+$result3 = $conn->query($query3) or die($this->_connect->error);
+
 while($row2 = $result3->fetch_array()){
     $resultNameCheck = $row2['cnt'];
 }
@@ -152,7 +154,7 @@ while($row2 = $result3->fetch_array()){
 
     <div id = 'resName' style="font-weight: bold; font-size: 20px;">
         <p>누적 테스터 의 수 : <?=$value1;?>명</p>
-<!--        <p>나와 같은 선택을 한 테스터의 수 : --><?//=$resultNameCheck-1;?><!--명</p>-->
+        <p>나와 같은 선택을 한 테스터의 수 : <?=$resultNameCheck-1;?>명</p>
     </div>
 
     <br>
