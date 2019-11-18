0b684b131c5a95ebdb3683e4a2571d6b1ad6b2f9
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-18 10:55:52 +0900

    결과 도출 수정

--- page_result.php
+++ page_result.php
@@ -173,20 +173,25 @@ for($i = 0; $i < sizeof($gradeArr); $i++){
 
 if($gradeArr[0] == 'C'){
     $res_style = $resultStyle[1];
+    $resVal = 2;
 } else if($gradeArr[1] == 'C'){
     $res_style = $resultStyle[2];
+    $resVal = 3;
 } else if($gradeArr[2] == 'C'){
     $res_style = $resultStyle[3];
+    $resVal = 4;
 } else if($gradeArr[3] == 'C'){
     $res_style = $resultStyle[4];
+    $resVal = 5;
 } else if($gradeArr[4] == 'C'){
     $res_style = $resultStyle[5];
+    $resVal = 6;
 }
-
 
 
 if($gradeArr[0] == 'C' && $gradeArr[1] == 'C' && $gradeArr[2] == 'C' && $gradeArr[2] == 'C' && $gradeArr[3] == 'C' && $gradeArr[4] == 'C'){
     $res_style = $resultStyle[7];
+    $resVal = 8;
 }
 
 if($_SERVER["REMOTE_ADDR"] == '211.52.72.56'){
@@ -196,8 +201,10 @@ if($_SERVER["REMOTE_ADDR"] == '211.52.72.56'){
         if($key !== 'C'){
             if(($key == 'A' && $value <= 5) || ($key == 'B' && $value >= 3)){
                 $res_style = $resultStyle[6];
+                $resVal = 7;
             } else {
                 $res_style = $resultStyle[0];
+                $resVal = 1;
             }
         }
     }
@@ -218,3 +225,96 @@ $result3 = $conn->query($query3) or die($this->_connect->error);
 //}
 
 ?>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8/jquery-ui.min.js"></script>
+<link rel="stylesheet" type="text/css" media="screen" href="http://ajax.googleapis.com/ajax/libs/jqueryui/1.8.14/themes/base/jquery-ui.css">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="/main.css">
+
+
+<script>
+
+    $(document).ready(function(){
+        var id = <?=$id;?>;
+        var result_style = '<?=$res_style;?>';
+        var resVal = <?=$resVal;?>;
+        var value = '';
+
+        $(".choice_div button").click(function () {
+            value = $(this).attr('value');
+
+            if(value){
+                window.location.href = 'http://34.80.159.83/page_5-3.php?id='+id+'&value='+value;
+            }
+        });
+        
+        // function getResultImg(resVal) {
+        //     var result = '';
+        //     if(resVal == '이상적 반려인'){
+        //         result = '<img src="/img/result/ed8.jpg">'
+        //     }
+        //     return result;
+        // }
+    });
+
+
+</script>
+
+<html>
+<title>
+    반려종 허용 테스트
+</title>
+<body>
+
+<div id = 'contentsDiv' style = "margin: auto; padding-top: 70px;width:1000px;padding-right: 100px;padding-left: 100px;">
+    <?php
+        if($res_style !== ''){?>
+            <div id = 'imgDiv'>
+                <img src="/img/result/ed<?=$resVal;?>.jpg">
+            </div>
+        <?}
+    ?>
+
+    <p>어느 날 큰 지진이 일어났다. 쓰나미가 일어나고 집이 무너지기 시작했다. 밖에서 큰 폭발음이 들렸다.<br>
+        나는 방공호로 대피했다. <br>
+        시간이 흐르고 잠잠해진 후, 밖으로 나온 나는 라디오의 신호를 가까스로 잡을 수 있었다.<br>
+        <br>
+        “살아남은 자들은 광장으로 모이세요"<br>
+        <br>
+        도시 외곽에 사는 나는 갈 길이 멀다. 나는 채비하고 집 밖으로 나왔다.<br>
+    </p>
+    <br>
+<!--    <form action="page_1-1.php" method="get">-->
+<!--        <div class = 'choice_div' style="margin: auto; width:50px;">-->
+<!--            <input type="hidden" name = 'id' value="--><?//=$id?><!--">-->
+<!--            <input type="submit" value="버려진 외곽으로 입장"><br>-->
+<!--        </div>-->
+<!--    </form>-->
+    <br>
+    <!--    <div class = 'moveBtn_div'>-->
+    <!---->
+    <!--        <input type="submit" value="뒤로" onclick="goBack()">-->
+    <!--    </div>-->
+</div>
+
+<!--<div class = "testBtn1" value = '1' style="border-left: 1px grey;">선택지1</div>-->
+<!--<div class = 'testBtn2'>-->
+<!--    <a href="/test_main_page_1.php" class="btn btn-secondary" value="1">선택지1</a>-->
+<!--</div>-->
+<!--<div>-->
+<!--    <ul>-->
+<!--        <li>1</li>-->
+<!--    </ul>-->
+<!--    <ul>-->
+<!--        <li>2</li>-->
+<!--    </ul>-->
+<!--</div>-->
+</body>
+</html>
+
