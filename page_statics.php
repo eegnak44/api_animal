1ee0acfd4807e52d73cfef7a123ab78971c285ea
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-18 11:30:56 +0900

    결과 도출 수정

--- page_statics.php
+++ page_statics.php
@@ -6,6 +6,52 @@
  * Date: 2019-11-14
  * Time: 오후 2:24
  */
+function isDevDebug()
+{
+
+    $dev_ip = array("211.52.72.51",
+        "211.52.72.56",
+        "211.52.72.59" );
+
+    return true;//in_array( $_SERVER["REMOTE_ADDR"], $dev_ip);
+}
+function debug_var($var = '')
+{
+
+    if(isDevDebug())
+    {
+        echo _before();
+        if (is_array($var))
+        {
+            print_r($var);
+        }
+        elseif (is_object($var))
+        {
+            print_r($var);
+        }
+        else
+        {
+            echo $var;
+        }
+        echo _after();
+    }
+}
+
+function _before()
+{
+    $before = '<div style="position:relative; z-index:999999; padding:10px 20px 10px 20px; background-color:#fbe6f2; border:1px solid #d893a1; color: #000; font-size: 12px;" class="Debug">'."\n";
+    $before .= '<h5 style="font-family:verdana,sans-serif; font-weight:bold; font-size:18px; margin:0px 0px 10px 0px;">Debug Helper Output</h5>'."\n";
+    $before .= '<xmp style="font-weight: bold;">'."\n";
+    return $before;
+}
+
+function _after()
+{
+    $after = '</xmp>'."\n";
+    $after .= '<h5 style="font-family:verdana,sans-serif; font-weight:bold; font-size:18px; margin:0px 0px 10px 0px;">END</h5>'."\n";
+    $after .= '</div>'."\n";
+    return $after;
+}
 
 $id = $_REQUEST['id'];
 $value = $_REQUEST['value'];
@@ -29,6 +75,7 @@ while($row = $result2->fetch_array())
     $value1 = $row['cnt'];
 }
 
+debug_var($row);
 ?>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
