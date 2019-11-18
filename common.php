0dbf3afddda000d3020ef0ad06c28bc845da43c6
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-18 09:37:40 +0900

    챕터 1 스트링 변경

--- common.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: chris
- * Date: 2019-11-14
- * Time: 오후 7:39
- */
-
-?>
--- page_result.php
+++ page_result.php
@@ -1,4 +1,5 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<? include "Common.php" ?>
 <?php
 /**
  * Created by PhpStorm.
@@ -7,52 +8,52 @@
  * Time: 오후 2:24
  */
 
-function isDevDebug()
-{
-
-    $dev_ip = array("211.52.72.51",
-        "211.52.72.56",
-        "211.52.72.59" );
-
-    return true;//in_array( $_SERVER["REMOTE_ADDR"], $dev_ip);
-}
-function debug_var($var = '')
-{
-
-    if(isDevDebug())
-    {
-        echo _before();
-        if (is_array($var))
-        {
-            print_r($var);
-        }
-        elseif (is_object($var))
-        {
-            print_r($var);
-        }
-        else
-        {
-            echo $var;
-        }
-        echo _after();
-    }
-}
-
-function _before()
-{
-    $before = '<div style="position:relative; z-index:999999; padding:10px 20px 10px 20px; background-color:#fbe6f2; border:1px solid #d893a1; color: #000; font-size: 12px;" class="Debug">'."\n";
-    $before .= '<h5 style="font-family:verdana,sans-serif; font-weight:bold; font-size:18px; margin:0px 0px 10px 0px;">Debug Helper Output</h5>'."\n";
-    $before .= '<xmp style="font-weight: bold;">'."\n";
-    return $before;
-}
-
-function _after()
-{
-    $after = '</xmp>'."\n";
-    $after .= '<h5 style="font-family:verdana,sans-serif; font-weight:bold; font-size:18px; margin:0px 0px 10px 0px;">END</h5>'."\n";
-    $after .= '</div>'."\n";
-    return $after;
-}
+//function isDevDebug()
+//{
+//
+//    $dev_ip = array("211.52.72.51",
+//        "211.52.72.56",
+//        "211.52.72.59" );
+//
+//    return true;//in_array( $_SERVER["REMOTE_ADDR"], $dev_ip);
+//}
+//function debug_var($var = '')
+//{
+//
+//    if(isDevDebug())
+//    {
+//        echo _before();
+//        if (is_array($var))
+//        {
+//            print_r($var);
+//        }
+//        elseif (is_object($var))
+//        {
+//            print_r($var);
+//        }
+//        else
+//        {
+//            echo $var;
+//        }
+//        echo _after();
+//    }
+//}
+//
+//function _before()
+//{
+//    $before = '<div style="position:relative; z-index:999999; padding:10px 20px 10px 20px; background-color:#fbe6f2; border:1px solid #d893a1; color: #000; font-size: 12px;" class="Debug">'."\n";
+//    $before .= '<h5 style="font-family:verdana,sans-serif; font-weight:bold; font-size:18px; margin:0px 0px 10px 0px;">Debug Helper Output</h5>'."\n";
+//    $before .= '<xmp style="font-weight: bold;">'."\n";
+//    return $before;
+//}
+//
+//function _after()
+//{
+//    $after = '</xmp>'."\n";
+//    $after .= '<h5 style="font-family:verdana,sans-serif; font-weight:bold; font-size:18px; margin:0px 0px 10px 0px;">END</h5>'."\n";
+//    $after .= '</div>'."\n";
+//    return $after;
+//}
 
 $id = $_REQUEST['id'];
 $value = $_REQUEST['value'];
@@ -200,7 +201,7 @@ if($_SERVER["REMOTE_ADDR"] == '211.52.72.56'){
 }
 
 
-echo $res_style;
+debug_var($res_style);
 
 
 $query3 = "UPDATE play_dataTB SET result_style = '{$res_style}' where ID = '{$id}'";
