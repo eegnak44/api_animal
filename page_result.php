ec7822346cefedbfa1ff7eb1ebfdd1b1d14987a7
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-19 17:30:47 +0900

    결과 도출 수정

--- page_result.php
+++ page_result.php
@@ -159,6 +159,7 @@ debug_var($gradeArr);
 
 if($gradeArr[0] == 'C'){
     $tmpQuery1 = "select one_to_one, one_to_two, one_to_three from play_dataTB where chapter_one = '{$resObj['chapter_one']}' and ID = '{$id}'";
+    echo $tmpQuery1;
     $res1 = $conn->query($tmpQuery1) or die($this->_connect->error);
     while($row1 = $res->fetch_array())
     {
@@ -168,6 +169,7 @@ if($gradeArr[0] == 'C'){
 }
 if($gradeArr[1] == 'C'){
     $tmpQuery2 = "select two_to_one, two_to_two, two_to_three from play_dataTB where chapter_two = '{$resObj['chapter_two']}' and ID = '{$id}'";
+    echo $tmpQuery2;
     $res2 = $conn->query($tmpQuery2) or die($this->_connect->error);
     while($row2 = $res2->fetch_array())
     {
@@ -177,6 +179,7 @@ if($gradeArr[1] == 'C'){
 }
 if($gradeArr[2] == 'C'){
     $tmpQuery3 = "select three_to_one, three_to_two, three_to_three from play_dataTB where chapter_three = '{$resObj['chapter_three']}' and ID = '{$id}'";
+    echo $tmpQuery3;
     $res3 = $conn->query($tmpQuery3) or die($this->_connect->error);
     while($row3 = $res->fetch_array())
     {
@@ -186,6 +189,7 @@ if($gradeArr[2] == 'C'){
 }
 if($gradeArr[3] == 'C'){
     $tmpQuery4 = "select four_to_one, four_to_two, four_to_three from play_dataTB where chapter_four = '{$resObj['chapter_four']}' and ID = '{$id}'";
+    echo $tmpQuery4;
     $res4 = $conn->query($tmpQuery4) or die($this->_connect->error);
     while($row4 = $res4->fetch_array())
     {
@@ -195,6 +199,7 @@ if($gradeArr[3] == 'C'){
 }
 if($gradeArr[4] == 'C'){
     $tmpQuery5 = "select five_to_one, five_to_two, five_to_three from play_dataTB where chapter_five = '{$resObj['chapter_five']}' and ID = '{$id}'";
+    echo $tmpQuery5;
     $res5 = $conn->query($tmpQuery5) or die($this->_connect->error);
     while($row5 = $res5->fetch_array())
     {
