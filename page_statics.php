10f46b8b61fa2595e861ea29fe87766baf39527e
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-20 10:43:22 +0900

    결과 도출 수정

--- page_statics.php
+++ page_statics.php
@@ -171,11 +171,15 @@ while($row2 = $result3->fetch_array()){
     </div>
     <div id = 'resCheck'>
         <?
-            for($i=0;$i<5;$i++){?>
+        for($j=0;$j<3;$j++){?>
+          <br>
+            <br>
+            <?for($i=0;$i<5;$i++){?>
                 <div style="float: left;"><img src="img/unnamed.png" style="width: 80px; margin: 0px;"></div>
-                <div style="float: left;">test</div
-            <?}?>
+                <div style="float: left;">test</div>
 
+            <?}
+        }?>
 
     </div>
     <br>
