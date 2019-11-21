526ce6cd1399b44d5d7318c0f69c40be826c7486
chris-at-982817383129 <N3Zd0P5q20dSo5k/xUcqyeGrYRrcsLV7rEkmgTN37qA=>
2019-11-21 12:55:01 +0900

    각 스테이지 스트링 수정

--- page_statics.php
+++ page_statics.php
@@ -93,19 +93,19 @@ $resultName = '';
 
 while($row1 = $result->fetch_array()) {
     $list1[0] = $row1[0];
-    $list1[1] = $row1[1];
-    $list1[2] = $row1[2];
-    $list1[3] = $row1[3];
-    $list1[4] = $row1[4];
-    $list2[0] = $row1[5];
-    $list2[1] = $row1[6];
+    $list2[0] = $row1[1];
+    $list3[0] = $row1[2];
+    $list1[1] = $row1[3];
+    $list2[1] = $row1[4];
+    $list3[1] = $row1[5];
+    $list1[2] = $row1[6];
     $list2[2] = $row1[7];
-    $list2[3] = $row1[8];
-    $list2[4] = $row1[9];
-    $list3[0] = $row1[10];
-    $list3[1] = $row1[11];
-    $list3[2] = $row1[12];
-    $list3[3] = $row1[13];
+    $list3[2] = $row1[8];
+    $list1[3] = $row1[9];
+    $list2[3] = $row1[10];
+    $list3[3] = $row1[11];
+    $list1[4] = $row1[12];
+    $list2[4] = $row1[13];
     $list3[4] = $row1[14];
     $resultName = $row1[15];
 //    debug_var($row1);
@@ -123,6 +123,8 @@ $targetArray = ['1','2','3','4'];
 $replaceArrayList1 = str_replace($targetArray, $replaceArr,$list1);
 $replaceArrayList2 = str_replace($targetArray, $replaceArr,$list2);
 $replaceArrayList3 = str_replace($targetArray, $replaceArr,$list3);
+//$replaceArrayList4 = str_replace($targetArray, $replaceArr,$list4);
+//$replaceArrayList5 = str_replace($targetArray, $replaceArr,$list5);
 
 //debug_var($replaceArrayList1);
 //debug_var($replaceArrayList2);
@@ -171,34 +173,34 @@ $result_1 = $conn->query($query_1) or die($this->_connect->error);
 while($row_1 = $result_1->fetch_array()){
     $res1[0] = $row_1['cnt'];
 }
-$query_2 = "select count({$tmpQueryCol1[1]}) as cnt from play_dataTB where one_to_two = '{$list1[1]}'";
+$query_2 = "select count({$tmpQueryCol1[1]}) as cnt from play_dataTB where one_to_two = '{$list2[0]}'";
 $result_2 = $conn->query($query_2) or die($this->_connect->error);
 while($row_2 = $result_2->fetch_array()){
     $res1[1] = $row_2['cnt'];
 }
-$query_3 = "select count({$tmpQueryCol1[2]}) as cnt from play_dataTB where one_to_three = '{$list1[2]}'";
+$query_3 = "select count({$tmpQueryCol1[2]}) as cnt from play_dataTB where one_to_three = '{$list3[0]}'";
 $result_3 = $conn->query($query_1) or die($this->_connect->error);
 while($row_3 = $result_3->fetch_array()){
     $res1[2] = $row_3['cnt'];
 }
-$query_4 = "select count({$tmpQueryCol1[3]}) as cnt from play_dataTB where two_to_one = '{$list1[3]}'";
+
+$query_4 = "select count({$tmpQueryCol1[3]}) as cnt from play_dataTB where two_to_one = '{$list1[1]}'";
 $result_4 = $conn->query($query_4) or die($this->_connect->error);
 while($row_4 = $result_4->fetch_array()){
     $res1[3] = $row_4['cnt'];
 }
-$query_5 = "select count({$tmpQueryCol1[4]}) as cnt from play_dataTB where two_to_two = '{$list1[4]}'";
+$query_5 = "select count({$tmpQueryCol1[4]}) as cnt from play_dataTB where two_to_two = '{$list2[1]}'";
 $result_5 = $conn->query($query_5) or die($this->_connect->error);
 while($row_5 = $result_5->fetch_array()){
     $res1[4] = $row_5['cnt'];
 }
-
-
-$query_6 = "select count({$tmpQueryCol2[0]}) as cnt from play_dataTB where two_to_three = '{$list2[0]}'";
+$query_6 = "select count({$tmpQueryCol2[0]}) as cnt from play_dataTB where two_to_three = '{$list3[1]}'";
 $result_6 = $conn->query($query_6) or die($this->_connect->error);
 while($row_6 = $result_6->fetch_array()){
     $res2[0] = $row_6['cnt'];
 }
-$query_7 = "select count({$tmpQueryCol2[1]}) as cnt from play_dataTB where three_to_one = '{$list2[1]}'";
+
+$query_7 = "select count({$tmpQueryCol2[1]}) as cnt from play_dataTB where three_to_one = '{$list1[2]}'";
 $result_7 = $conn->query($query_7) or die($this->_connect->error);
 while($row_7 = $result_7->fetch_array()){
     $res2[1] = $row_7['cnt'];
@@ -208,34 +210,35 @@ $result_8 = $conn->query($query_8) or die($this->_connect->error);
 while($row_8 = $result_8->fetch_array()){
     $res2[2] = $row_8['cnt'];
 }
-$query_9 = "select count({$tmpQueryCol2[3]}) as cnt from play_dataTB where three_to_three = '{$list2[3]}'";
+$query_9 = "select count({$tmpQueryCol2[3]}) as cnt from play_dataTB where three_to_three = '{$list3[2]}'";
 $result_9 = $conn->query($query_9) or die($this->_connect->error);
 while($row_9 = $result_9->fetch_array()){
     $res2[3] = $row_9['cnt'];
 }
-$query_10 = "select count({$tmpQueryCol2[4]}) as cnt from play_dataTB where four_to_one = '{$list2[4]}'";
+
+
+$query_10 = "select count({$tmpQueryCol2[4]}) as cnt from play_dataTB where four_to_one = '{$list1[3]}'";
 $result_10 = $conn->query($query_10) or die($this->_connect->error);
 while($row_10 = $result_10->fetch_array()){
     $res2[4] = $row_10['cnt'];
 }
-
-
-$query_11 = "select count({$tmpQueryCol3[0]}) as cnt from play_dataTB where four_to_two = '{$list3[0]}'";
+$query_11 = "select count({$tmpQueryCol3[0]}) as cnt from play_dataTB where four_to_two = '{$list2[3]}'";
 $result_11 = $conn->query($query_11) or die($this->_connect->error);
 while($row_11 = $result_11->fetch_array()){
     $res3[0] = $row_11['cnt'];
 }
-$query_12 = "select count({$tmpQueryCol3[1]}) as cnt from play_dataTB where four_to_three = '{$list3[1]}'";
+$query_12 = "select count({$tmpQueryCol3[1]}) as cnt from play_dataTB where four_to_three = '{$list3[3]}'";
 $result_12 = $conn->query($query_12) or die($this->_connect->error);
 while($row_12 = $result_12->fetch_array()){
     $res3[1] = $row_12['cnt'];
 }
-$query_13 = "select count({$tmpQueryCol3[2]}) as cnt from play_dataTB where five_to_one = '{$list3[2]}'";
+
+$query_13 = "select count({$tmpQueryCol3[2]}) as cnt from play_dataTB where five_to_one = '{$list1[4]}'";
 $result_13 = $conn->query($query_13) or die($this->_connect->error);
 while($row_13 = $result_13->fetch_array()){
     $res3[2] = $row_13['cnt'];
 }
-$query_14 = "select count({$tmpQueryCol3[3]}) as cnt from play_dataTB where five_to_two = '{$list3[3]}'";
+$query_14 = "select count({$tmpQueryCol3[3]}) as cnt from play_dataTB where five_to_two = '{$list2[4]}'";
 $result_14 = $conn->query($query_14) or die($this->_connect->error);
 while($row_14 = $result_14->fetch_array()){
     $res3[3] = $row_14['cnt'];
@@ -248,11 +251,18 @@ while($row_15 = $result_15->fetch_array()){
 
 
 
+$stageResNameKr1 = ["병든<br>떠돌이개", "다른 역사", "다른 정치관","초파리","고도의<br>지능과<br>감정을<br>지닌 로봇"];
+$stageResNameKr2 = ["새끼를<br>밴<br>길고양이", "다른 민족","전통주의","거미","유전자<br>변형<br>괴생명체"];
+$stageResNameKr3 = ["방사능<br>피폭자","다른 종교","진보주의","바퀴벌레","과학기술과<br> 특이<br>바이러스를<br>가진 외계인"];
 
+//$stageResNameKr1 = ["병든<br>떠돌이개","새끼를<br>밴<br>길고양이","방사능<br>피폭자"];
+//$stageResNameKr2 = ["다른 역사","다른 민족","다른 종교"];
+//$stageResNameKr3 = ["다른 정치관","전통주의","진보주의"];
+//$stageResNameKr4 = ["초파리","거미","바퀴벌레"];
+//$stageResNameKr5 = ["고도의<br>지능과<br>감정을<br>지닌 로봇","유전자<br>변형<br>괴생명체","과학기술과<br> 특이<br>바이러스를<br>가진 외계인"];
 
-$stageResNameKr1 = ["병든<br>떠돌이개","새끼를<br>밴<br>길고양이","방사능<br>피폭자","통일<br>한국의<br>북한 인민","불법체류<br>외국노동자"];
-$stageResNameKr2 = ["이슬람<br>난민", "정치<br>견해가<br>다른 사람","안티<br>페미니스트",'페미니스트','초파리'];
-$stageResNameKr3 = ['거미','바퀴벌레','고도의<br>지능과<br>감정을<br>지닌 로봇','유전자<br>변형<br>괴생명체','과학기술과<br> 특이<br>바이러스를<br>가진 외계인'];
+//$stageResNameKr2 = ["이슬람<br>난민", "정치<br>견해가<br>다른 사람","안티<br>페미니스트",'페미니스트','초파리'];
+//$stageResNameKr3 = ['거미','바퀴벌레','고도의<br>지능과<br>감정을<br>지닌 로봇','유전자<br>변형<br>괴생명체','과학기술과<br> 특이<br>바이러스를<br>가진 외계인'];
 ?>
 
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -303,8 +313,7 @@ $stageResNameKr3 = ['거미','바퀴벌레','고도의<br>지능과<br>감정을
 
         <div id = 'resCheck' style="width: 1000px">
             <?
-            for($i=0;$i<5;$i++){?>
-                <div style="float: left;"><img src="img/unnamed.png" style="width: 91px; margin: 0px;"></div>
+            for($i=0;$i<3;$i++){?>
                 <div style="float: left; width: 91px;"><?=$stageResNameKr1[$i]?><p style="font-weight: bold;"><?=$replaceArrayList1[$i]?><br><?=$res1[$i];?>명</p></div>
             <?}?>
         </div>
@@ -312,16 +321,13 @@ $stageResNameKr3 = ['거미','바퀴벌레','고도의<br>지능과<br>감정을
         <div id = 'resCheck' style="width: 1000px; float: left;">
             <?
             for($i=0;$i<5;$i++){?>
-                <div style="float: left;"><img src="img/unnamed.png" style="width: 91px; margin: 0px;"></div>
                 <div style="float: left; width: 91px;"><?=$stageResNameKr2[$i]?><p style="font-weight: bold"><?=$replaceArrayList2[$i]?><br><?=$res2[$i];?>명</p></div>
-
             <?}?>
         </div>
         <br>
         <div id = 'resCheck' style="width: 1000px">
             <?
             for($i=0;$i<5;$i++){?>
-                <div style="float: left;"><img src="img/unnamed.png" style="width: 91px; margin: 0px;"></div>
                 <div style="float: left; width: 91px;"><?=$stageResNameKr3[$i]?><p style="font-weight: bold"><?=$replaceArrayList3[$i]?><br><?=$res3[$i];?>명</p></div>
             <?}?>
         </div>
